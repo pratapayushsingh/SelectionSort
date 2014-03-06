c399fa1f879e106f7b7b2d85bbe456a3ddf0609e
SkylaBlue <skylablue1337@hotmail.com>
2014-03-06 05:53:50 -0700

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -18,11 +18,6 @@ typedef unsigned int UINT;
 void selectionSort(int array[], UINT size)
 {
     // first iteration to find the lowest value
-	for (UINT i = 0; i < size; i++)
-	{typedef unsigned int UINT;
-void selectionSort(int array[], UINT size)
-{
-    // first iteration to find the lowest value
   for (UINT i = 0; i < size; i++)
   {
     if (array[0] > array[i])
@@ -59,43 +54,4 @@ int main()
   }
   
   return 0;
-}
-SkylaBlue
-Commit new file
-
-		if (array[0] > array[i])
-		{
-			int temp = array[0];
-			array[0] = array[i];
-			array[i] = temp;
-		}
-	}
-	
-	for (UINT i = 1; i < size; i++)
-	{
-		for (UINT j = i+1; j < size; j++)
-		{
-			if (array[i] > array[j])
-			{
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-}
-
-int main()
-{
-	int test[11] = {3, 6, 1, 4, 0, 8, 2, 9, 10, 5, 7};
-	UINT usize  = sizeof(test) / sizeof(test[0]);
-	selectionSort(test, usize);
-	
-	
-	for (UINT i = 0; i < usize; ++i)
-	{
-		std::cout << test[i] << std::endl;
-	}
-	
-	return 0;
 }
